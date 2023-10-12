cb1f98a564e1a9c46eb00aac3f3d26ad9e9348f9
iammyeongho <jmh4912@naver.com>
2023-10-12 18:06:29 +0900

    1012 list 수정

--- src/php/list.php
+++ src/php/list.php
@@ -210,7 +210,7 @@
 									<img src="/1st_project/src/img/stupid.png">
 								<?php } ?>
 							</td>
-							<td class="content-title-box"><a href="/1st_project/src/php/datail.php/?id=<?php echo $item["id"]; ?>&date =<?php echo $date;?>"><?php echo $item["title"]?></a></td>
+							<td class="content-title-box"><a href="/1st_project/src/php/datail.php/?id=<?php echo $item["id"]; ?>&date=<?php echo $date;?>"><?php echo $item["title"]?></a></td>
 							<td class="content-amount-box"><?php echo $item["amount_used"], "원"; ?></td>
 						</tr>
 						<?php 
