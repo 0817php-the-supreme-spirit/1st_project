72e30bc60b2af9f127e5e0eb2b80be534e939cd1
iammyeongho <jmh4912@naver.com>
2023-10-16 16:01:09 +0900

    1016 css php 통합

--- src/php/side.php
+++ src/php/side.php
@@ -58,6 +58,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/1st_project/src/css/side/style.css">
 	<title>Document</title>
 </head>
 <body>
